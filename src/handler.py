bd0029d7295ebb91dc598a7ca8aa61c09033e094
supa-thibaud <thibaud@supafriends.com>
2024-08-23 19:19:40 +0200

    async_v2

--- src/handler.py
+++ src/handler.py
@@ -17,27 +17,26 @@ async def async_handler(job):
     
     if job_input.get("openai_route"):
         openai_route, openai_input = job_input.get("openai_route"), job_input.get("openai_input")
-        async with OpenAIRequest() as openai_request:
-            if openai_route == "/v1/chat/completions":
-                async for chunk in openai_request.request_chat_completions(**openai_input):
-                    yield chunk
-            elif openai_route == "/v1/completions":
-                async for chunk in openai_request.request_completions(**openai_input):
-                    yield chunk
-            elif openai_route == "/v1/models": 
-                models = await openai_request.get_models()
-                yield models
+        openai_request = OpenAIRequest()
+        
+        if openai_route == "/v1/chat/completions":
+            async for chunk in openai_request.request_chat_completions(**openai_input):
+                yield chunk
+        elif openai_route == "/v1/completions":
+            async for chunk in openai_request.request_completions(**openai_input):
+                yield chunk
+        elif openai_route == "/v1/models":
+            models = await openai_request.get_models()
+            yield models
     else:
-        yield {"error": "Unsupported request type", "details": "Only OpenAI-compatible routes are supported"}
+        async for result in engine.generate(job_input):
+            yield result
 
 max_concurrency = int(os.getenv("MAX_CONCURRENCY", 100))
+print(f"MAX_CONCURRENCY {max_concurrency}")
+
 runpod.serverless.start({
     "handler": async_handler, 
-    "concurrency_modifier": lambda x: max_concurrency,
-
+    "concurrency_modifier": lambda x: max_concurrency, 
     "return_aggregate_stream": True
-    })
-
-# Ensure the server is shut down when the serverless function is terminated
-import atexit
-atexit.register(engine.shutdown)
+})
